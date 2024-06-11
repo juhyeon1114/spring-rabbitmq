95dd2a41700d7a36c11f03d568163900f2c04d6e
juhyeon <juhyeon1114@gmail.com>
2024-06-11 17:44:54 +0900

    클러스터 추가 스크립트

---
+++ cluster_join.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+docker exec -it rabbitmq-2 rabbitmqctl stop_app
+docker exec -it rabbitmq-2 rabbitmqctl reset
+docker exec -it rabbitmq-2 rabbitmqctl join_cluster rabbit@rabbitmq-1
+docker exec -it rabbitmq-2 rabbitmqctl start_app
+
+docker exec -it rabbitmq-3 rabbitmqctl stop_app
+docker exec -it rabbitmq-3 rabbitmqctl reset
+docker exec -it rabbitmq-3 rabbitmqctl join_cluster rabbit@rabbitmq-1
+docker exec -it rabbitmq-3 rabbitmqctl start_app
